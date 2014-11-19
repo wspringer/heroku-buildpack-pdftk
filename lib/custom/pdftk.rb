87c188e4028512b590c09840af53ec0fc0059a24
Mark Madsen <mm@idyll.io>
2014-11-19 16:35:31 -0700

    Change the pdftk source again.

--- lib/custom/pdftk.rb
+++ lib/custom/pdftk.rb
@@ -9,7 +9,7 @@ class Pdftk < BaseCustom
   end
 
   def source_url
-    "https://github.com/SirRawlins/pdftk-source/raw/master/pdftk.tar.gz"
+    "https://raw.githubusercontent.com/SirRawlins/pdftk-source/master/pdftk.tar.gz"
   end
 
   def used?
